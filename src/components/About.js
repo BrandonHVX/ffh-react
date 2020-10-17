25fce4f7e46ebf3ccf9738e4fe1f3d34c8f33698
Brandon <brandongines@gmail.com>
2020-10-17 00:57:46 -0400

    layout

--- src/components/About.js
+++ src/components/About.js
@@ -21,8 +21,7 @@ function About() {
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit,
                     sed do eiusmod tempor incididunt ut labore et dolore magna
                     aliqua. Ut enim ad minim enim veniam, quis nostrud
-                    exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat.
+                    exercitation ullamco laboris nisi ut aliquip ex ea commodo.
                   </p>
                 </header>
               </div>
